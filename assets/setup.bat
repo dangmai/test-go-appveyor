012643a9be7efd7eca45777191ef2dfcf01e4ba0
Dang Mai <Dang.Mai@TheMentorNetwork.com>
2018-03-05 12:23:47 -0500

    Use pathed to manipulate path

--- assets/setup.bat
+++ assets/setup.bat
@@ -28,7 +28,7 @@ IF %ERRORLEVEL% NEQ 0 (
     REM Install Chocolatey
     @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 )
-choco install -y msys2 rktools.2003
+choco install -y msys2
 C:\tools\msys64\usr\bin\bash.exe -l -c "pacman -Syu --noconfirm --ask=20 mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-pkg-config git"
 
 SETX ORACLE_HOME %ORACLE_HOME%
@@ -36,5 +36,5 @@ SETX OCI_LIB %ORACLE_HOME%\sdk\lib
 SETX TNS_ADMIN %ORACLE_HOME%\network\admin
 SETX PKG_CONFIG %ORACLE_HOME%
 
-pathman /au C:\tools\msys64\mingw64\bin
-pathman /au C:\tools\msys64\usr\bin
+pathed /APPEND C:\tools\msys64\mingw64\bin /USER
+pathed /APPEND C:\tools\msys64\usr\bin /USER
