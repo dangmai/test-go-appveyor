8f82c4e831c1b19f527e187107bb62716a5947b0
Dang Mai <Dang.Mai@TheMentorNetwork.com>
2018-03-05 12:01:14 -0500

    Take out Exit statement

--- assets/setup.bat
+++ assets/setup.bat
@@ -39,5 +39,3 @@ SETX PKG_CONFIG %ORACLE_HOME%
 REM Set the user's PATH. We can't just setx PATH %PATH% because %PATH% contains both user and system values, which will bring system values into the user's PATH
 for /f "usebackq tokens=2,*" %A in (`reg query HKCU\Environment /v PATH`) do set USER_PATH=%B
 setx PATH "%USER_PATH%;C:\tools\msys64\mingw64\bin;C:\tools\msys64\usr\bin"
-
-EXIT /B 0
